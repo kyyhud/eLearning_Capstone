2f90ef2d30177a8a0b5c6c007cfe1e4514103a07
kyyhud <kyyhud@gmail.com>
2026-09-11 17:28:08 -0500

    feat: update application title to LearnEase Pro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h2>eLearning App - Capstone Project</h2>
+      <h2>LearnEase Pro - Capstone Project</h2>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
